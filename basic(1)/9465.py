12a02d638be5c18d35856eb271b1070fab4a6567
sangyunpark99 <psangyun5108@gmail.com>
2023-04-25 21:28:08 +0900

    9465번

--- basic(1)/9465.py
+++ basic(1)/9465.py
@@ -1,5 +1,3 @@
-import sys
-
 T = int(input())
 
 for _ in range(T):
@@ -9,9 +7,11 @@ for _ in range(T):
     if n > 1:
         dp[0][1] += dp[1][0]
         dp[1][1] += dp[0][0]
+        for i in range(2, n):
+            for j in range(2):
+                if j == 0:
+                    dp[j][i] = max(dp[j + 1][i - 1] + dp[j][i], dp[j + 1][i - 2] + dp[j][i])
+                else:
+                    dp[j][i] = max(dp[j - 1][i - 1] + dp[j][i], dp[j - 1][i - 2] + dp[j][i])
 
-    for i in range(2,n):
-        dp[0][i] += max(dp[1][i-1],dp[i][i-2])
-        dp[1][i] += max(dp[0][i-1],dp[0][i-2])
-
-    print(max(dp[0][n-1], dp[1][n-1]))
+    print(max(dp[0][-1], dp[1][-1]))
